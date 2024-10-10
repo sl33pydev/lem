f5cca813b80ba5196af32965aaf6d49fe86e9318
Luca Giabbani <sl33py@baseddeartment2.fibertel.com.ar>
2024-10-10 05:50:49 -0300

    Fixed ruby-calc-indent

--- extensions/ruby-mode/ruby-mode.lisp
+++ extensions/ruby-mode/ruby-mode.lisp
@@ -1,5 +1,8 @@
 (defpackage :lem-ruby-mode
   (:use :cl :lem :lem/language-mode :lem/language-mode-tools)
+  (:import-from :lem-js-mode
+                :get-line-indent
+                :move-to-previous-line)
   (:export :ruby-mode))
 
 (in-package :lem-ruby-mode)
@@ -10,7 +13,8 @@
     "end" "next" 
     "redo" "retry"
     "return" "self" "super"
-    "undef" "yield"))
+    "undef" "yield" "private"
+    "public" "protected"))
 
 (defvar *ruby-block-beg-keywords*
   '("class" "module" "def" "case" "for" "begin" "do"))
@@ -24,6 +28,13 @@
 (defvar *ruby-block-op-keywords*
   '("and" "or" "not"))
 
+(defvar *ruby-block-start*
+  "(^|\\s)(def|class|module|loop|for|begin|do)\\b")
+
+;; I Need a regex for keywords that could be at the end of a line, such as
+;; while|if|unless|until|case|when
+(defvar *ruby-block-end*
+  "(^|\\s)(end)\\b")
 
 (defvar *ruby-boolean-literals*
   '("true" "false"))
@@ -92,14 +103,15 @@
      :syntax-table *ruby-syntax-table*
      :mode-hook *ruby-mode-hook*)
   (setf (variable-value 'enable-syntax-highlight) t
-        (variable-value 'indent-tabs-mode) nil
-        (variable-value 'tab-width) 2
-        (variable-value 'line-comment) "#"
-        (variable-value 'beginning-of-defun-function) 'beginning-of-defun
-        (variable-value 'end-of-defun-function) 'end-of-defun))
+		(variable-value 'indent-tabs-mode) nil 
+		(variable-value 'calc-indent-function) 'ruby-calc-indent
+		(variable-value 'tab-width) 2
+		(variable-value 'line-comment) "#"
+		(variable-value 'beginning-of-defun-function) 'beginning-of-defun
+		(variable-value 'end-of-defun-function) 'end-of-defun))
 
 (defun beginning-of-defun (point n)
-  (loop :with regex = "^(def|class|module|case|for|begin|do|if|unless|while|until)?"
+  (loop :with regex = *ruby-block-start*
         :repeat n 
         :do (search-backward-regexp point regex)))
 
@@ -111,5 +123,46 @@
               (unless (search-forward-regexp p "^  end") (return)))
     (line-start p)
     (move-point point p)))
+
+
+;; https://rubystyle.guide/ to determine indentation (this could be changed)
+;; IDEA--- Instead of getting the current indent, i should check the previous indentation
+;; and decide wether or not i should increase it or decrease it
+
+(defun ruby-calc-indent (point)
+  (with-point ((prev point))
+    (move-to-previous-line prev)
+    ;; What happens when we are in the first line
+    (with-point ((p point))
+      (if (start-buffer-p (line-start p))
+          (return-from ruby-calc-indent 0)))
+    (let ((tab-width (variable-value 'tab-width :default point))
+          (column (length (get-line-indent prev))))
+      ;; when in a comment
+    (when (in-string-or-comment-p point)
+      (with-point ((p point))
+        (back-to-indentation p)
+        (return-from ruby-calc-indent (point-column p))))
+
+    ;; What happens if the previous line has a start of block keyword
+    (with-point ((p point))
+      (when (move-to-previous-line p)
+        (with-point ((start p)
+                     (end p))
+          (line-start start)
+          (line-end end)
+          (when (search-backward-regexp end *ruby-block-start* start)
+            (return-from ruby-calc-indent (incf column tab-width)) 
+            ))))
+    
+      ;; what happens if the current line has an end keyword
+      (with-point ((p point)
+                 (end point))
+      (if (search-forward-regexp p "\\b(end)" (line-end end))
+          (decf column tab-width)
+          )
+      )
+    column))
+  )
 
 (define-file-type ("rb" "rspec" "Gemfile") ruby-mode)
